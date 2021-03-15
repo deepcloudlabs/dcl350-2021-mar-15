46375c3785432328102e42d0943e54c27aa65ee6
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-15 16:12:36 +0300

    add initial domain model

---
+++ hr-hexagon/src/com/example/hr/domain/Employee.java
@@ -0,0 +1,16 @@
+package com.example.hr.domain;
+
+
+//Entity Class -> Identity
+public class Employee {
+	private TcKimlikNo identity;
+	private FullName fullname;
+	private Money salary;
+	private Iban iban;
+	private BirthYear birthYear;
+	private Department department;
+	private JobType jobType;
+	private Photo photo;
+	
+
+}
---
+++ hr-hexagon/src/com/example/hr/domain/Iban.java
@@ -0,0 +1,5 @@
+package com.example.hr.domain;
+
+public class Iban {
+
+}
