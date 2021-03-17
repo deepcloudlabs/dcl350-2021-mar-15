4706a63aab8b09d4dde502c26322f92aa797a96e
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-17 15:35:31 +0300

    add configuration

---
+++ spring-boot-rest-api-websocket-client/src/main/java/com/example/SpringBootRestApiWebsocketClientApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiWebsocketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiWebsocketClientApplication.class, args);
+	}
+
+}
