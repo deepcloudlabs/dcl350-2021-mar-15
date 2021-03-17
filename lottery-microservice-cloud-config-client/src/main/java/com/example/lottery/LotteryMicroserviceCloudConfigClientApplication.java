99f595fad3d88f486a0e07a4e88bbb04565cd6b8
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-17 20:00:35 +0300

    add configuration

---
+++ lottery-microservice-cloud-config-client/src/main/java/com/example/lottery/LotteryMicroserviceCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryMicroserviceCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryMicroserviceCloudConfigClientApplication.class, args);
+	}
+
+}
