73fce2055c95a85a338026ea96e821f01f29e36a
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-17 15:35:14 +0300

    study config server and client

--- lottery-microservice/src/main/java/com/example/lottery/service/business/StandardLotteryService.java
+++ lottery-microservice/src/main/java/com/example/lottery/service/business/StandardLotteryService.java
@@ -5,11 +5,13 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.example.lottery.service.LotteryService;
 
 @Service
+@RefreshScope
 public class StandardLotteryService implements LotteryService {
 	@Value("${lottery.max}") // SpEL
 	private int lotteryMax;
