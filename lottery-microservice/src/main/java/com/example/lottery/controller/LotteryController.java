7afc760689e9287d2e88146c9ff16fd67ae72e6f
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-17 14:18:02 +0300

    add lottery microservice study application.properties and @Value injection

---
+++ lottery-microservice/src/main/java/com/example/lottery/controller/LotteryController.java
@@ -0,0 +1,28 @@
+package com.example.lottery.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+
+import com.example.lottery.service.LotteryService;
+
+@RestController
+@RequestScope
+@RequestMapping("numbers")
+@CrossOrigin
+public class LotteryController {
+
+	@Autowired
+	private LotteryService lotteryService;
+
+	// http://localhost:7700/lottery/api/v1/numbers
+	@GetMapping
+	public List<Integer> getNumbers(){
+		return lotteryService.draw();
+	}
+}
