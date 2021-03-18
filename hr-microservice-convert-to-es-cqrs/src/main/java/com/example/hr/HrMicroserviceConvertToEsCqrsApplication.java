3f0cdceb8fc81c3cb4f1864078254021eb540415
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-18 16:41:45 +0300

    initial commit

---
+++ hr-microservice-convert-to-es-cqrs/src/main/java/com/example/hr/HrMicroserviceConvertToEsCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMicroserviceConvertToEsCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrMicroserviceConvertToEsCqrsApplication.class, args);
+	}
+
+}
