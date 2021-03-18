c7cf4a7fb7dbaccc991d638cc3e18c3ea105d0b2
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-18 16:42:12 +0300

    remove warning

--- lottery-consumer/src/main/java/com/example/lottery/service/LotteryConsumerService.java
+++ lottery-consumer/src/main/java/com/example/lottery/service/LotteryConsumerService.java
@@ -8,7 +8,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
