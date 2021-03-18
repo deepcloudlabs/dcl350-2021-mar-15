f535a7b3ac650e795aff4bc70e60a04e51de31fe
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-18 11:02:44 +0300

    remove error

--- lottery-consumer/src/main/java/com/example/lottery/service/client/LotteryService.java
+++ lottery-consumer/src/main/java/com/example/lottery/service/client/LotteryService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "lottery-v1",fallback = )
+@FeignClient(name = "lottery-v1" )
 public interface LotteryService {
 	@GetMapping("/lottery/api/v1/numbers")
 	List<Integer> draw();
